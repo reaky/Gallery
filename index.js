3291bb0472b9002a7c30ea1ae04ffb9ab847c409
Reaky <reaky.ye@gmail.com>
2016-03-20 21:44:48 +0800

    fix items url

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ var openPhotoSwipe = function(items, pid) {
 	var pswpElement = document.querySelectorAll('.pswp')[0];
 
 	items = arguments[0];
-	for (var i=0;i<items.length;i++){
-		items[i].src = "http://7xrst7.com1.z0.glb.clouddn.com/" + items[i].src
-	};
 	pid = arguments[1] || 0;
 	// define options (if needed)
 	var options = {
@@ -24,8 +21,11 @@ $(document).ready(function(){
 		dataType: "json",
 		success: function (items) {
 			console.log("load meta.json finished");
+			for (var i=0;i<items.length;i++){
+				items[i].src = "http://7xrst7.com1.z0.glb.clouddn.com/" + items[i].src
+			};
 			openPhotoSwipe(items, 0);
-			$("#Reaky-Gallery").html($('<img src="photos/IMG_20160208_105111.JPG" width=100% height=100% />'));
+			$("#Reaky-Gallery").html($('<img src="http://7xrst7.com1.z0.glb.clouddn.com/photos/IMG_20160208_105111.JPG" width=100% height=100% />'));
 			$('#Reaky-Gallery > img:last').click(function(){openPhotoSwipe(items,0)});
 			//$.each(items, function(index, item){
 			//	$("#Reaky-Gallery").append($('<img src='+item['src']+' alt='+item['src']+' />'));
