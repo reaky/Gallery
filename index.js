d92fb110f307f0d16a446989acff9f00e57cf7f3
reaky <reaky.ye@gmail.com>
2016-06-19 22:22:10 +0800

    update photo source

--- index.js
+++ index.js
@@ -17,23 +17,25 @@ var openPhotoSwipe = function(items, pid) {
 
 $(document).ready(function(){
 	$.ajax({
-		url: 'http://7xrst7.com1.z0.glb.clouddn.com/list.json?attname=',
+		url: 'http://photo.reaky.xyz/list.json',
 		dataType: "json",
 		success: function (items) {
 			console.log("load meta.json finished");
 			for (var i=0;i<items.length;i++){
-				items[i].src = "http://7xrst7.com1.z0.glb.clouddn.com/" + items[i].src
+				items[i].src = "http://photo.reaky.xyz/photos/" + items[i].src;
+				items[i].w = 800;
+				items[i].h = 800;
 			};
 			openPhotoSwipe(items, 0);
-			$("#Reaky-Gallery").html($('<img src="http://7xrst7.com1.z0.glb.clouddn.com/photos/IMG_20160208_105111.JPG" width=100% height=100% />'));
-			$('#Reaky-Gallery > img:last').click(function(){openPhotoSwipe(items,0)});
-			//$.each(items, function(index, item){
-			//	$("#Reaky-Gallery").append($('<img src='+item['src']+' alt='+item['src']+' />'));
-			//	$('#Reaky-Gallery > img:last').click({value: index}, function(event){
-			//			openPhotoSwipe(items, event.data.value);
-			//		});
-			//	console.log(index);
-			//});
+			//$("#Reaky-Gallery").html($('<img src="http://7xrst7.com1.z0.glb.clouddn.com/photos/IMG_20160208_105111.JPG" width=100% height=100% />'));
+			//$('#Reaky-Gallery > img:last').click(function(){openPhotoSwipe(items,0)});
+			$.each(items, function(index, item){
+				$("#Reaky-Gallery").append($('<img src='+item['src']+' alt='+item['src']+' />'));
+				$('#Reaky-Gallery > img:last').click({value: index}, function(event){
+						openPhotoSwipe(items, event.data.value);
+					});
+				console.log(index);
+			});
 		},
 		error: function (err) {
 			console.log("load meta.json failed");
